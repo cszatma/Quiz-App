70c269f03bbaa9ef56caa9ef64b2bd37f23848ff
cszatma <christopherszatmary@gmail.com>
2017-04-07 20:45:18 -0400

    Initial Commit

---
+++ Quiz AppTests/Quiz_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  Quiz_AppTests.swift
+//  Quiz AppTests
+//
+//  Created by Christopher Szatmary on 2017-04-07.
+//  Copyright © 2017 Christopher Szatmary. All rights reserved.
+//
+
+import XCTest
+@testable import Quiz_App
+
+class Quiz_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
