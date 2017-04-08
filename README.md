ec038ef7790aa315739e192d3629848e3ea4d657
cszatma <christopherszatmary@gmail.com>
2017-04-08 18:37:54 -0400

    Adding files

---
+++ README.md
@@ -0,0 +1,3 @@
+# Quiz-App
+
+Fetches multiple choice questions from a server and displays them for the user to answer.
---
+++ SDKs/CSKitUniversal.framework/Headers/CSKitUniversal.h
@@ -0,0 +1,19 @@
+//
+//  CSKitUniversal.h
+//  CSKitUniversal
+//
+//  Created by Christopher Szatmary on 2017-04-08.
+//  Copyright © 2017 Christopher Szatmary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CSKitUniversal.
+FOUNDATION_EXPORT double CSKitUniversalVersionNumber;
+
+//! Project version string for CSKitUniversal.
+FOUNDATION_EXPORT const unsigned char CSKitUniversalVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CSKitUniversal/PublicHeader.h>
+
+
