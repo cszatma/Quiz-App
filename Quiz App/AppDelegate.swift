65376e358c69d457caca2d743b7e66b065d0f2e3
cszatma <christopherszatmary@gmail.com>
2017-04-17 15:07:33 -0400

    Removed unnecessary comments.

--- Quiz App/AppDelegate.swift
+++ Quiz App/AppDelegate.swift
@@ -16,11 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Design //
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         window?.rootViewController = UINavigationController(rootViewController: MenuViewController())
-        // End //
         FIRApp.configure()
         FIRDatabase.database().persistenceEnabled = true
         return true
