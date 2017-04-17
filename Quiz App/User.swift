9c8e6f5bbb4f8d469133a9932faf7e5b4424e168
cszatma <christopherszatmary@gmail.com>
2017-04-17 00:38:53 -0400

    Created user datatype.

--- Quiz App/User.swift
+++ Quiz App/User.swift
@@ -6,4 +6,40 @@
 //  Copyright © 2017 Christopher Szatmary. All rights reserved.
 //
 
-import Foundation
+import Firebase
+
+struct User {
+    let name: String
+    let email: String
+    let score: Int
+    let questionsAnswered: Int
+    let ref: FIRDatabaseReference?
+    let uid: String?
+    
+    init(name: String, email: String, score: Int, questionsAnswered: Int) {
+        self.name = name
+        self.email = email
+        self.score = score
+        self.questionsAnswered = questionsAnswered
+        self.ref = nil
+        self.uid = nil
+    }
+    
+    init(snapshot: FIRDataSnapshot) {
+        let values = snapshot.value as! [String: Any]
+        name = values["name"] as! String
+        email = values["email"] as! String
+        score = values["score"] as! Int
+        questionsAnswered = values["questionsAnswered"] as! Int
+        ref = snapshot.ref
+        uid = snapshot.key
+    }
+    
+//    init(authUser: FIRUser) {
+//        self.email = authUser.email!
+//    }
+    
+    func toJSON() -> Any {
+        return ["name": name, "email": email, "score": score, "questionsAnswered": questionsAnswered]
+    }
+}
