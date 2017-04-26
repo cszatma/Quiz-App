108867990d9464809ee61d6e502b20ab959772a2
cszatma <christopherszatmary@gmail.com>
2017-04-25 23:24:48 -0400

    Created Settings and Stats ViewControllers. Created QAController Protocol.

---
+++ Quiz App/QAController.swift
@@ -0,0 +1,9 @@
+//
+//  QAController.swift
+//  Quiz App
+//
+//  Created by Christopher Szatmary on 2017-04-25.
+//  Copyright © 2017 Christopher Szatmary. All rights reserved.
+//
+
+import Foundation
